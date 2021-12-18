90521c9edaede0693c1666174319cad78908ada2
facupaez <facu.pz10@gmail.com>
2021-12-18 15:46:17 -0300

    api key deleted

--- src/main/java/com/facu/disney/service/impl/EmailServiceImpl.java
+++ src/main/java/com/facu/disney/service/impl/EmailServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmalServiceImpl implements EmailService {
+public class EmailServiceImpl implements EmailService {
 
     @Autowired
     private Environment env;
